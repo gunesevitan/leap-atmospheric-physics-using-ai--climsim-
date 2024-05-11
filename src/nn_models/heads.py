918f08698a08d252119706e6991437df1ef90efb
gunesevitan <gunesevitan@gmail.com>
2024-05-11 09:13:22 +0300

    multi head

--- src/nn_models/heads.py
+++ src/nn_models/heads.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -16,35 +17,37 @@ class SingleOutputHead(nn.Module):
         return outputs
 
 
-class MultiTaskHead(nn.Module):
+class MultiOutputHead(nn.Module):
 
     def __init__(self, input_dim):
 
-        super(MultiTaskHead, self).__init__()
+        super(MultiOutputHead, self).__init__()
 
-        self.ptend_t_head = nn.Linear(input_dim, 60, bias=False)
-        self.ptend_q0001_head = nn.Linear(input_dim, 60, bias=False)
-        self.ptend_q0002_head = nn.Linear(input_dim, 60, bias=False)
-        self.ptend_q0003_head = nn.Linear(input_dim, 60, bias=False)
-        self.ptend_u_head = nn.Linear(input_dim, 60, bias=False)
-        self.ptend_v_head = nn.Linear(input_dim, 60, bias=False)
-        self.cam_out_NETSW_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_FLWDS_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_PRECSC_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_PRECC_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_SOLS_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_SOLL_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_SOLSD_head = nn.Linear(input_dim, 1, bias=False)
-        self.cam_out_SOLLD_head = nn.Linear(input_dim, 1, bias=False)
+        self.ptend_t_head = nn.Conv1d(input_dim, out_channels=60, kernel_size=(1,), bias=True)
+        self.ptend_q0001_head = nn.Conv1d(input_dim, out_channels=60, kernel_size=(1,), bias=True)
+        self.ptend_q0002_head = nn.Conv1d(input_dim, out_channels=60, kernel_size=(1,), bias=True)
+        self.ptend_q0003_head = nn.Conv1d(input_dim, out_channels=60, kernel_size=(1,), bias=True)
+        self.ptend_u_head = nn.Conv1d(input_dim, out_channels=60, kernel_size=(1,), bias=True)
+        self.ptend_v_head = nn.Conv1d(input_dim, out_channels=60, kernel_size=(1,), bias=True)
+        self.cam_out_NETSW_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_FLWDS_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_PRECSC_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_PRECC_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_SOLS_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_SOLL_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_SOLSD_head = nn.Linear(input_dim, 1, bias=True)
+        self.cam_out_SOLLD_head = nn.Linear(input_dim, 1, bias=True)
 
     def forward(self, x):
 
-        ptend_t_outout = self.ptend_t_head(x)
-        ptend_q0001_outout = self.ptend_q0001_head(x)
-        ptend_q0002_outout = self.ptend_q0002_head(x)
-        ptend_q0003_outout = self.ptend_q0003_head(x)
-        ptend_u_outout = self.ptend_u_head(x)
-        ptend_v_outout = self.ptend_v_head(x)
+        x_conv = torch.unsqueeze(x, dim=1).permute(0, 2, 1)
+
+        ptend_t_outout = torch.squeeze(self.ptend_t_head(x_conv), -1)
+        ptend_q0001_outout = torch.squeeze(self.ptend_q0001_head(x_conv), -1)
+        ptend_q0002_outout = torch.squeeze(self.ptend_q0002_head(x_conv), -1)
+        ptend_q0003_outout = torch.squeeze(self.ptend_q0003_head(x_conv), -1)
+        ptend_u_outout = torch.squeeze(self.ptend_u_head(x_conv), -1)
+        ptend_v_outout = torch.squeeze(self.ptend_v_head(x_conv), -1)
         cam_out_NETSW_outout = self.cam_out_NETSW_head(x)
         cam_out_FLWDS_outout = self.cam_out_FLWDS_head(x)
         cam_out_PRECSC_outout = self.cam_out_PRECSC_head(x)
