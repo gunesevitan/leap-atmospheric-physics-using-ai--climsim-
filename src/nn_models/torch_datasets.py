e5bf64a845cd8f1cf934a7a35ce2ea10eed1e483
gunesevitan <gunesevitan@gmail.com>
2024-05-11 09:13:38 +0300

    datasets

--- src/nn_models/torch_datasets.py
+++ src/nn_models/torch_datasets.py
@@ -44,11 +44,11 @@ class TabularDataset(Dataset):
 
         Returns
         -------
-        features: dict of 25 torch.Tensor of shape 60 or 1
-            Dictionary of feature tensors
+        features: torch.Tensor of shape (556)
+            Features tensor
 
-        targets: dict of 14 torch.Tensor of shape 60 or 1
-            Dictionary of target tensors
+        targets: torch.Tensor of shape (368)
+            Targets tensor
         """
 
         features = np.load(self.feature_paths[idx])['arr']
@@ -59,6 +59,7 @@ class TabularDataset(Dataset):
         )
 
         if self.target_paths is not None:
+
             targets = np.load(self.target_paths[idx])['arr']
             targets = get_target_tensors(
                 targets=targets,
@@ -67,14 +68,16 @@ class TabularDataset(Dataset):
             )
 
             return features, targets
+
         else:
+
             return features
 
 
 def get_feature_tensors(features, feature_statistics=None, feature_normalization_type=None):
 
     """
-    Create dictionary of feature tensors
+    Create inputs tensors
 
     Parameters
     ----------
@@ -89,8 +92,8 @@ def get_feature_tensors(features, feature_statistics=None, feature_normalization
 
     Returns
     -------
-    features: dict of 25 torch.Tensor of shape 60 or 1
-        Dictionary of feature tensors
+    features: torch.Tensor of shape (556)
+        Features tensor
     """
 
     if feature_statistics is not None:
@@ -101,34 +104,7 @@ def get_feature_tensors(features, feature_statistics=None, feature_normalization
         else:
             raise ValueError(f'Invalid normalization type {feature_normalization_type}')
 
-    features = {
-        'state_t': features[0:60],
-        'state_q0001': features[60:120],
-        'state_q0002': features[120:180],
-        'state_q0003': features[180:240],
-        'state_u': features[240:300],
-        'state_v': features[300:360],
-        'state_ps': features[360],
-        'pbuf_SOLIN': features[361],
-        'pbuf_LHFLX': features[362],
-        'pbuf_SHFLX': features[363],
-        'pbuf_TAUX': features[364],
-        'pbuf_TAUY': features[365],
-        'pbuf_COSZRS': features[366],
-        'cam_in_ALDIF': features[367],
-        'cam_in_ALDIR': features[368],
-        'cam_in_ASDIF': features[369],
-        'cam_in_ASDIR': features[370],
-        'cam_in_LWUP': features[371],
-        'cam_in_ICEFRAC': features[372],
-        'cam_in_LANDFRAC': features[373],
-        'cam_in_OCNFRAC': features[374],
-        'cam_in_SNOWHLAND': features[375],
-        'pbuf_ozone': features[376:436],
-        'pbuf_CH4': features[436:496],
-        'pbuf_N2O': features[496:556],
-    }
-    features = {k: torch.as_tensor(v, dtype=torch.float).reshape(-1) for k, v in features.items()}
+    features = torch.as_tensor(features, dtype=torch.float)
 
     return features
 
@@ -136,7 +112,7 @@ def get_feature_tensors(features, feature_statistics=None, feature_normalization
 def get_target_tensors(targets, target_statistics=None, target_normalization_type=None):
 
     """
-    Create dictionary of target tensors
+    Create outputs tensor
 
     Parameters
     ----------
@@ -151,8 +127,8 @@ def get_target_tensors(targets, target_statistics=None, target_normalization_typ
 
     Returns
     -------
-    targets: dict of 14 torch.Tensor of shape 60 or 1
-        Dictionary of target tensors
+    targets: torch.Tensor of shape (368)
+        Targets tensor
     """
 
     if target_statistics is not None:
@@ -163,23 +139,7 @@ def get_target_tensors(targets, target_statistics=None, target_normalization_typ
         else:
             raise ValueError(f'Invalid normalization type {target_normalization_type}')
 
-    targets = {
-        'ptend_t': targets[0:60],
-        'ptend_q0001': targets[60:120],
-        'ptend_q0002': targets[120:180],
-        'ptend_q0003': targets[180:240],
-        'ptend_u': targets[240:300],
-        'ptend_v': targets[300:360],
-        'cam_out_NETSW': targets[360],
-        'cam_out_FLWDS': targets[361],
-        'cam_out_PRECSC': targets[362],
-        'cam_out_PRECC': targets[363],
-        'cam_out_SOLS': targets[364],
-        'cam_out_SOLL': targets[365],
-        'cam_out_SOLSD': targets[366],
-        'cam_out_SOLLD': targets[367],
-    }
-    targets = {k: torch.as_tensor(v, dtype=torch.float).reshape(-1) for k, v in targets.items()}
+    targets = torch.as_tensor(targets, dtype=torch.float)
 
     return targets
 
