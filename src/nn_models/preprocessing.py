8dbb96336fd5fc091c4b8aefa8c9afe36820a3bb
gunesevitan <gunesevitan@gmail.com>
2024-05-24 09:51:12 +0300

    pp

--- src/nn_models/preprocessing.py
+++ src/nn_models/preprocessing.py
@@ -1,3 +1,79 @@
+import numpy as np
+
+
+def load_normalizers(normalizer_directory):
+
+    """
+    Load normalizers from given directory
+
+    Parameters
+    ----------
+    normalizer_directory: str or pathlib.Path
+        Path of the normalizer directory
+
+    Returns
+    -------
+    normalizers: dict
+        Dictionary of normalization statistics and objects
+    """
+
+    feature_means = np.load(normalizer_directory / 'feature_means.npy')
+    feature_stds = np.load(normalizer_directory / 'feature_stds.npy')
+    feature_mins = np.load(normalizer_directory / 'feature_mins.npy')
+    feature_maxs = np.load(normalizer_directory / 'feature_maxs.npy')
+    feature_rmss = np.load(normalizer_directory / 'feature_rmss.npy')
+
+    feature_log_means = np.load(normalizer_directory / 'feature_log_means.npy')
+    feature_log_stds = np.load(normalizer_directory / 'feature_log_stds.npy')
+    feature_log_mins = np.load(normalizer_directory / 'feature_log_mins.npy')
+    feature_log_maxs = np.load(normalizer_directory / 'feature_log_maxs.npy')
+    feature_log_rmss = np.load(normalizer_directory / 'feature_log_rmss.npy')
+
+    target_weights = np.load(normalizer_directory / 'target_weights.npy')
+
+    target_means = np.load(normalizer_directory / 'target_means.npy')
+    target_stds = np.load(normalizer_directory / 'target_stds.npy')
+    target_mins = np.load(normalizer_directory / 'target_mins.npy')
+    target_maxs = np.load(normalizer_directory / 'target_maxs.npy')
+    target_rmss = np.load(normalizer_directory / 'target_rmss.npy')
+
+    target_log_means = np.load(normalizer_directory / 'target_log_means.npy')
+    target_log_stds = np.load(normalizer_directory / 'target_log_stds.npy')
+    target_log_mins = np.load(normalizer_directory / 'target_log_mins.npy')
+    target_log_maxs = np.load(normalizer_directory / 'target_log_maxs.npy')
+    target_log_rmss = np.load(normalizer_directory / 'target_log_rmss.npy')
+
+    normalizers = {
+        'feature': {
+            'mean': feature_means,
+            'std': feature_stds,
+            'min': feature_mins,
+            'max': feature_maxs,
+            'rms': feature_rmss,
+            'log_mean': feature_log_means,
+            'log_std': feature_log_stds,
+            'log_min': feature_log_mins,
+            'log_max': feature_log_maxs,
+            'log_rms': feature_log_rmss,
+        },
+        'target': {
+            'weight': target_weights,
+            'mean': target_means,
+            'std': target_stds,
+            'min': target_mins,
+            'max': target_maxs,
+            'rms': target_rmss,
+            'log_mean': target_log_means,
+            'log_std': target_log_stds,
+            'log_min': target_log_mins,
+            'log_max': target_log_maxs,
+            'log_rms': target_log_rmss,
+        }
+    }
+
+    return normalizers
+
+
 def get_target_column_groups(columns):
 
     """
