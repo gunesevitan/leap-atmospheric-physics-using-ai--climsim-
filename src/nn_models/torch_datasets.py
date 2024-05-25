23d772eb0ee5599f40a42180eb48ef4ef044afe8
gunesevitan <gunesevitan@gmail.com>
2024-05-25 10:31:02 +0300

    dataset

--- src/nn_models/torch_datasets.py
+++ src/nn_models/torch_datasets.py
@@ -74,6 +74,60 @@ class TabularDataset(Dataset):
             return features
 
 
+class TabularInMemoryDataset(Dataset):
+
+    def __init__(self, features, targets=None):
+
+        self.features = features
+        self.targets = targets
+
+    def __len__(self):
+
+        """
+        Get the length the dataset
+
+        Returns
+        -------
+        length: int
+            Length of the dataset
+        """
+
+        return len(self.features)
+
+    def __getitem__(self, idx):
+
+        """
+        Get the idxth element in the dataset
+
+        Parameters
+        ----------
+        idx: int
+            Index of the sample (0 <= idx < length of the dataset)
+
+        Returns
+        -------
+        features: torch.Tensor of shape (556)
+            Features tensor
+
+        targets: torch.Tensor of shape (368)
+            Targets tensor
+        """
+
+        features = self.features[idx]
+        features = torch.as_tensor(features, dtype=torch.float)
+
+        if self.targets is not None:
+
+            targets = self.targets[idx]
+            targets = torch.as_tensor(targets, dtype=torch.float)
+
+            return features, targets
+
+        else:
+
+            return features
+
+
 def get_feature_tensors(features, feature_statistics=None, feature_normalization_type=None):
 
     """
