274c3ecac1c5137224fca48c61f7f68e72eb96b2
gunesevitan <gunesevitan@gmail.com>
2024-05-28 17:28:19 +0300

    npy writer

--- src/dataset_utilities/numpy_writer.py
+++ src/dataset_utilities/numpy_writer.py
@@ -1,6 +1,7 @@
 import sys
 from tqdm import tqdm
 import numpy as np
+import polars as pl
 
 sys.path.append('..')
 import settings
@@ -27,9 +28,15 @@ if __name__ == '__main__':
             for idx, line in enumerate(tqdm(f, total=10091520)):
 
                 ln = line.split(',')
-                features = np.array(ln[1:557], dtype=np.float32)
+                features = np.array(ln[1:557], dtype=np.float64)
+                features = np.concatenate((
+                    features[:360].reshape(-1, 60),
+                    np.expand_dims(features[360:376], axis=-1).repeat(repeats=60, axis=-1),
+                    features[376:].reshape(-1, 60),
+                ), axis=0)
+
                 np.savez_compressed(training_features_directory / f'{idx}.npz', arr=features)
-                targets = np.array(ln[557:], dtype=np.float32)
+                targets = np.array(ln[557:], dtype=np.float64)
                 np.savez_compressed(training_targets_directory / f'{idx}.npz', arr=targets)
 
     elif dataset == 'test':
@@ -40,5 +47,42 @@ if __name__ == '__main__':
 
             for idx, line in enumerate(tqdm(f, total=625000)):
                 ln = line.split(',')
-                features = np.array(ln[1:557], dtype=np.float32)
+                features = np.array(ln[1:557], dtype=np.float64)
+                features = np.concatenate((
+                    features[:360].reshape(-1, 60),
+                    np.expand_dims(features[360:376], axis=-1).repeat(repeats=60, axis=-1),
+                    features[376:].reshape(-1, 60),
+                ), axis=0)
                 np.savez_compressed(test_features_directory / f'{idx}.npz', arr=features)
+
+    elif dataset == 'train_features':
+
+        columns = np.arange(1, 557).tolist()
+        dtypes = [pl.Float32 for i in range(len(columns))]
+        features = pl.read_csv(
+            settings.DATA / 'leap-atmospheric-physics-ai-climsim' / 'train.csv',
+            columns=columns,
+            dtypes=dtypes,
+            n_threads=16
+        ).to_numpy()
+
+        features = np.concatenate((
+            features[:, :360].reshape(features.shape[0], -1, 60),
+            np.expand_dims(features[:, 360:376], axis=-1).repeat(repeats=60, axis=-1),
+            features[:, 376:].reshape(features.shape[0], -1, 60),
+        ), axis=1)
+
+        np.savez_compressed(settings.DATA / 'datasets' / 'train_features.npz', arr=features)
+
+    elif dataset == 'train_targets':
+
+        columns = np.arange(1, 369).tolist()
+        dtypes = [pl.Float32 for i in range(len(columns))]
+        targets = pl.read_csv(
+            settings.DATA / 'leap-atmospheric-physics-ai-climsim' / 'train.csv',
+            columns=columns,
+            dtypes=dtypes,
+            n_threads=16
+        ).to_numpy()
+
+        np.savez_compressed(settings.DATA / 'datasets' / 'train_targets.npz', arr=targets)
