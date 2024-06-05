b9c8efd1d26572215677786d9dc16170ed5cd0cc
gunesevitan <gunesevitan@gmail.com>
2024-06-05 08:43:39 +0300

    m

--- src/nn_models/torch_modules.py
+++ src/nn_models/torch_modules.py
@@ -2,3 +2,7 @@ from torch.nn import *
 
 from mlp import MLP
 from kan import KAN
+from resnet1d import ResNet1D
+from unet import UNetSeResNet
+from rnn import LSTM
+from transformer import Transformer
